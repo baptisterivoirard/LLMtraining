e77a07718d34e76d7827d0a492fe1c749ce43bb2
Baptiste Rivoirard <rivoirardbaptiste2@gmail.com>
2025-07-29 17:55:28 +0200

    prepare main.py

--- main.py
+++ main.py
@@ -5,25 +5,13 @@ import sys
 import time
 import os
 from dotenv import load_dotenv
-from emailrecup import connect_to_email, affiche_info_email
+from emailrecup import email_recup
 
 def main ():
     while True:
-        mail_ids, mail = connect_to_email()
-        if not mail_ids:
-            print("Pas de nouveaux mails.")
-            time.sleep(60)
-        list_mails =[]
-        for id in mail_ids:
-            msg_data = mail.fetch(id, "(RFC822)")
-            raw_email=msg_data[0][1]
-            if isinstance(raw_email, bytes):
-                email_message = email.message_from_bytes(raw_email)
-            else:
-                email_message = email.message_from_string(raw_email)
-            list_mails.append(affiche_info_email(email_message))
-            print("nombre de mails en attente de traitement : ", len(list_mails))
-            print(list_mails)
+        list_mails=[]
+        list_mails=email_recup()
+        print(list_mails)
         
 
         time.sleep(60)
---
+++ test.py
@@ -0,0 +1,9 @@
+prompt = [
+        { "role": "system",
+        "content": "You are a specialized email response assistant. Your task is to provide an optimised and personalized human-like response to the email given by the user.",
+        "role": "user"
+        }
+    ]
+print(prompt)
+prompt[0]["contente"]="test"
+print(prompt)
